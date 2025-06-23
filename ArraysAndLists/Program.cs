827f0e44eb04bd7c118e2369e241dbd4b655e1bb
Shaun Rudisail <sprudisail@gmail.com>
2025-06-23 07:19:57 -0400

    Completed Arrays and Lists Project.

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 
 namespace ArraysAndLists
 {
@@ -9,19 +10,25 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int array and populate it with numbers 1-10
-            
+
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
 
             /* Create a list of type int
              * Name the list "evens"
              */
 
-            
+            var evens = new List<int>();
+
+
             /* Create another list of type int
              * Name the list "odds"
              */
-            
 
-             /* Using either a foreach or for loop,
+            var odds = new List<int>();
+
+
+            /* Using either a foreach or for loop,
              * iterate through the array you populated with 10 numbers.
              * Inside the scope of the loop,
              * check to see if each number in the array is even or odd.
@@ -29,16 +36,40 @@ namespace ArraysAndLists
              * If the number is odd, add it to the odds list.
              */
 
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evens.Add(number);
+                }
+                else
+                {
+                    odds.Add(number);
+                }
+            }
+
 
 
             /* Using a foreach loop,
              * display the numbers in your "evens" list
              */
-            
+
+            foreach (var number in evens)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("");
+
 
             /* Using a for loop,
              * display the numbers in your "odds" list
              */
+
+            foreach (var number in odds)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
